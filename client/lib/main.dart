72955aa96fa139e0c0760edf05618dee3caedda2
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-08-02 13:52:20 +0900

    중앙정렬

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -42,6 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             viewModel.isLoading ? const CircularProgressIndicator() : Container(),
             ElevatedButton(
